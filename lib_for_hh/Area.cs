1a8cb3fb90bd80f7cea4c92488f9ee53d7d7100f
TimurAym <58203643+TimurAym@users.noreply.github.com>
2020-06-13 18:43:42 +0300

    Add files via upload

---
+++ lib_for_hh/Area.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace lib_for_hh
+{
+    public class Area
+    {
+        public double areaRound(double radius)
+            {
+                double S = Math.PI*Math.Pow(radius,2);
+                return S;
+            }
+        public double areaTriangle(double a, double b, double c)
+        {
+            double p = (a + b + c) / 2;
+            double S = Math.Sqrt(p*(p-a)*(p-b)*(p-c));
+            if (a > b && a > c && (Math.Pow(a,2) ==( Math.Pow(b, 2)+ Math.Pow(c, 2))))
+            {
+                Console.WriteLine("Прямоугольный");
+            }
+            else if(b > a && b > c && (Math.Pow(b, 2) == (Math.Pow(a, 2) + Math.Pow(c, 2))))
+            {
+                Console.WriteLine("Прямоугольный");
+            }
+            else if (c > a && c > b && (Math.Pow(c, 2) == (Math.Pow(a, 2) + Math.Pow(b, 2))))
+            {
+                Console.WriteLine("Прямоугольный");
+            }
+            return S;
+           
+        }
+    }
+}
